84226daa4f551e2cfe46ac349ec9503b78e03b19
Colin Campbell <colin@elusivelabs.com>
2015-04-03 22:58:26 -0400

    User model

--- models/user.js
+++ models/user.js
@@ -1,25 +1,18 @@
 var bcrypt = require('bcrypt-nodejs');
 
 module.exports = function(sequelize, DataTypes) {
-    var user = sequelize.define('User'), 
-        {
-            email: DataTypes.STRING,
-            password: DataTypes.STRING,
-        },
-        {
-            instanceMethods: {
-                generateHash: function(password) {
-                    return bcrypt.hasSync(
-                        password, 
-                        bcrypt.genSaltSync(10), 
-                        null
-                    );
-                },
-                authenticate: function(password) {
-                    return bcrypt.compareSync(password, this.password); 
-                }
-            } 
-        }
-    );
+    var User = sequelize.define('User', {
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {
+        classMethods: {
+            generateHash: function(password) {
+                return bcrypt.hasSync( password, bcrypt.genSaltSync(10), null);
+            },
+            authenticate: function(password) {
+                return bcrypt.compareSync(password, this.password); 
+            }
+        } 
+    });
     return User;
 };
