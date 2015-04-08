79ddd4f221904b5220dc40a2eb0d3670df4a270e
Colin Campbell <colin@elusivelabs.com>
2015-04-08 12:29:32 -0400

    Login now working.

--- config/passport.js
+++ config/passport.js
@@ -30,11 +30,11 @@ module.exports = function(passport) {
         });
     });
 
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+  // =========================================================================
+  // LOCAL SIGNUP ============================================================
+  // =========================================================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
 
   passport.use('local-signup', new LocalStrategy({
     // by default, local strategy uses username and password, we will override with email
@@ -43,7 +43,7 @@ module.exports = function(passport) {
     passReqToCallback : true // allows us to pass back the entire request to the callback
   },
   function(req, email, password, done) {
-    user = User.find({where: {'email': email}}).success(function(user) {
+    User.find({where: {'email': email}}).success(function(user) {
       if (user) {
         return done(null, false, req.flash(
           'signupMessage', 
@@ -57,30 +57,25 @@ module.exports = function(passport) {
       });
   }));
 
-    passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) { // callback with email and password from our form
+  passport.use('local-login', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, email, password, done) { // callback with email and password from our form
+    User.find({where: {'email' : email}}).success(function(user) {
 
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        // User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error before anything else
-            // if (err)
-                // return done(err);
+      if (!user || !user.authenticate(password)) {
+        console.log('auth issue');
+        return done(null, false, req.flash('loginMessage', 'Incorrect login information.'));
+      } 
 
-            // if no user is found, return the message
-            // if (!user || !user.validPassword(password))
-                // return done(null, false, req.flash('loginMessage', 'Incorrect login information.')); // req.flash is the way to set flashdata using connect-flash
-
-            // all is well, return successful user
-            // return done(null, user);
-        // });
-
-    }));
+      return done(null, user); 
+    }).error(function(err) {
+        return done(err);
+    });
+  }));
     
     // route middleware to make sure a user is logged in
     function isLoggedIn(req, res, next) {
--- models/user.js
+++ models/user.js
@@ -1,18 +1,20 @@
 var bcrypt = require('bcrypt-nodejs');
 
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define('User', {
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
-    }, {
-        classMethods: {
-            generateHash: function(password) {
-                return bcrypt.hashSync( password, bcrypt.genSaltSync(10), null);
-            },
-            authenticate: function(password) {
-                return bcrypt.compareSync(password, this.password); 
-            }
-        } 
-    });
-    return User;
+  var User = sequelize.define('User', {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+  }, {
+      classMethods: {
+        generateHash: function(password) {
+          return bcrypt.hashSync( password, bcrypt.genSaltSync(10), null);
+        },
+      },
+      instanceMethods: {
+        authenticate: function(password) {
+          return bcrypt.compareSync(password, this.password); 
+        },
+      }
+  });
+  return User;
 };
