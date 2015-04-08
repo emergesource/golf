317710a93c8ab5eda1510e93d7bda4f590a30ede
Colin Campbell <colin@elusivelabs.com>
2015-04-08 11:21:41 -0400

    Fixed up the user signup.

--- config/passport.js
+++ config/passport.js
@@ -36,62 +36,26 @@ module.exports = function(passport) {
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) {
-        // asynchronous
-        // User.findOne wont fire unless data is sent back
-        process.nextTick(function() {
-          user = User.find({where: {'email': email}});
-
-          if (user) {
-            return done(null, false, req.flash(
-              'signupMessage', 
-              'That email is already taken.'
-            ));
-          }
-
+  passport.use('local-signup', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, email, password, done) {
+    user = User.find({where: {'email': email}}).success(function(user) {
+      if (user) {
+        return done(null, false, req.flash(
+          'signupMessage', 
+          'That email is already taken.'
+        ));
+      } else {
           User.create({'email' : email, 'password': User.generateHash(password)}).then(function(user) {
             return done(null, user);
           });
-
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        // User.findOne({ 'email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            // if (err)
-                // return done(err);
-
-            // check to see if theres already a user with that email
-            // if (user) {
-                // return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            // } else {
-
-                // if there is no user with that email
-                // create the user
-                // var newUser            = new User();
-
-                // set the user's local credentials
-                // newUser.email    = email;
-                // newUser.password = newUser.generateHash(password);
-
-                // save the user
-                // newUser.save(function(err) {
-                    // if (err)
-                        // throw err;
-                    // return done(null, newUser);
-                // });
-            // }
-
-        // });    
-
-        });
-
-    }));
+        }
+      });
+  }));
 
     passport.use('local-login', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
